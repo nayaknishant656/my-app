8e07eb62a804b59685d25940442f7b46ee258a0d
Nishant nayak <nayaknishant656@gmail.com>
2023-07-25 00:04:02 +0530

    Day-1 Porfolio

---
+++ src/components/body/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function main() {
+  return (
+    <div className='px-8'>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+    </div>
+  )
+}
+
+export default main
